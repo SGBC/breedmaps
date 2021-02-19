758c9254763db7ee0df3c90cbd1f2ef3928e95ba
Jenny Jakobsson <jenny.jakobsson.8203@student.uu.se>
2021-02-19 11:05:30 +0100

    Plotting of the vcf files and some filtering

---
+++ SVs_identification/scripts/python/plot_all_vcf.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import glob
+import matplotlib.pyplot as plt
+from functions import read_survivor_stats, summarize_stats, get_sample_name, plot_stats
+
+stats_files = glob.glob("/Users/jj/breedmaps/SVs_identification/results/SURVIVOR/*.stats")
+
+for i in range(len(stats_files)):
+    file_path = stats_files[i]
+    stats = read_survivor_stats(file_path)
+    sample_name = get_sample_name(file_path)
+    sum_stats = summarize_stats(stats)
+    plot_stats(sum_stats, sample_name)
+
+plt.legend(loc='upper center', ncol=5)
+plt.title('Number of SVs found in each genome')
+plt.show()
+
+
--- SVs_identification/scripts/python/sum_all_vcf.py
+++ SVs_identification/scripts/python/sum_all_vcf.py
@@ -1,8 +1,13 @@
-from functions import read_delly_vcf, filter_precise
+from functions import read_delly_vcf, filter_precise, filter_qual
 import glob
 vcf_files = glob.glob("/Users/jj/breedmaps/SVs_identification/data/vcf/*.vcf")
 vcf = [None] * len(vcf_files)
 for i in range(len(vcf_files)):
-    new_vcf = read_delly_vcf(vcf_files[i])
-    vcf[i] = filter_precise(new_vcf)
+    file = read_delly_vcf(vcf_files[i])
+    filtered = filter_qual(file)
+    precise = filter_precise(filtered)
+    vcf[i] = precise
+    print(f"The file has gone from {len(file)} to {len(vcf[i])} by filtering by FILTER and PRECISE flag\n")
+
+
 
